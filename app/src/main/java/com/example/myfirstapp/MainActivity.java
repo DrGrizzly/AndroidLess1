807e5e136852eb6d9111a0c0afb5ceef810191bc
Farhad <dr_grizzly@mail.ru>
2021-03-27 16:25:08 +0500

    Поправил замечания

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -12,12 +12,9 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_calc);
-
     }
 }
